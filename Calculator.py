aaebad5e84221110aac792017c86ac90c18aeae3
harshpratap0106 <harshpratap.2723@gmail.com>
2024-11-24 19:50:04 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+
+#Calculator 
+def sum(num1,num2):  
+    return num1+num2 
+def sub(num1,num2):
+    return num1-num2
+def multiplication(num1,num2):
+    return num1*num2
+def div(num1,num2):
+    if num2==0:
+         return "Error: Divide by Zero"
+    return num1/num2
+
+def calculator():
+     num1=float(input("Enter the 1st number:"))
+     num2=float(input("Enter the 2nd number:"))
+     operation=input("Enter the operation (+, -, *, /): ")
+     if operation == '+': 
+         print(f"{num1} + {num2} = {sum(num1, num2)}") 
+     elif operation == '-':
+         print(f"{num1} - {num2} = {sub(num1, num2)}") 
+     elif operation == '*': 
+         print(f"{num1} * {num2} = {multiplication(num1, num2)}") 
+     elif operation == '/':
+         print(f"{num1} / {num2} = {div(num1, num2)}") 
+     else: print("Invalid operation")
+calculator()
